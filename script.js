41232794e2e3ed366fdb5873c177536dfc6a7a18
Victor <victor901575@gmail.com>
2024-05-01 15:15:07 -0300

    vamos

--- script.js
+++ script.js
@@ -6,39 +6,39 @@ if (!publicKey) {
   console.error('Chave pública do Mercado Pago não foi definida.');
 }
 
-// Inicializar o Mercado Pago
-Mercadopago.setPublishableKey(publicKey);
-
 // Verificar se o Mercado Pago SDK foi carregado corretamente
-if (!Mercadopago) {
+if (typeof Mercadopago === 'undefined') {
   console.error('SDK do Mercado Pago não foi carregado corretamente.');
-}
+} else {
+  // Inicializar o Mercado Pago
+  Mercadopago.setPublishableKey(publicKey);
 
-document.getElementById('generate-qr-code').addEventListener('click', function() {
-  console.log('Botão de gerar QR Code clicado.'); // Verificar se o evento de clique está sendo acionado
+  document.getElementById('generate-qr-code').addEventListener('click', function() {
+    console.log('Botão de gerar QR Code clicado.');
 
-  const amount = document.getElementById('amount').value;
+    const amount = document.getElementById('amount').value;
 
-  // Chamar a API do Mercado Pago para gerar o QR Code
-  Mercadopago.createPayment({
-    transaction_amount: Number(amount),
-    description: 'Descrição do pagamento',
-    payment_method_id: 'qr',
-  }, function(response) {
-    if (response.status === 200) {
-      const qrCode = response.body.secure_thumbnail;
-      document.getElementById('qr-code').innerHTML = `<img src="${qrCode}" alt="QR Code">`;
-    } else {
-      alert('Erro ao gerar QR Code');
-    }
+    // Chamar a API do Mercado Pago para gerar o QR Code
+    Mercadopago.createPayment({
+      transaction_amount: Number(amount),
+      description: 'Descrição do pagamento',
+      payment_method_id: 'qr',
+    }, function(response) {
+      if (response.status === 200) {
+        const qrCode = response.body.secure_thumbnail;
+        document.getElementById('qr-code').innerHTML = `<img src="${qrCode}" alt="QR Code">`;
+      } else {
+        alert('Erro ao gerar QR Code');
+      }
+    });
   });
-});
 
-document.getElementById('card-payment').addEventListener('click', function() {
-  console.log('Botão de pagamento com cartão de crédito clicado.'); // Verificar se o evento de clique está sendo acionado
+  document.getElementById('card-payment').addEventListener('click', function() {
+    console.log('Botão de pagamento com cartão de crédito clicado.');
 
-  const amount = document.getElementById('amount').value;
+    const amount = document.getElementById('amount').value;
 
-  // Redirecionar para a página de pagamento do Mercado Pago para pagamento com cartão de crédito
-  window.location.href = `https://www.mercadopago.com/mlb/checkout/start?pref_id=${amount}`;
-});
+    // Redirecionar para a página de pagamento do Mercado Pago para pagamento com cartão de crédito
+    window.location.href = `https://www.mercadopago.com/mlb/checkout/start?pref_id=${amount}`;
+  });
+}
